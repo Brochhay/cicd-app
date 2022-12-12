f9a5325f1a227729f09c3d6342b6b164a0675284
brochhay <vanchhaycute168@gmail.com>
2022-12-12 13:10:16 +0700

    initial commit

---
+++ cicd/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React in super Cool </h1>
+    </div>
+  );
+}
+
+export default App;
